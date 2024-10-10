0708681dfbeefff3ca12b3cc4027b3f74cf29512
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2024-10-10 23:49:23 +0100

    new change

---
+++ src/connector/retrieveTransactions/retrieveTransactions.ts
@@ -0,0 +1,11 @@
+import connectorHelper from '../connector-helper';
+
+const getTransactions = async () =>
+  await connectorHelper(
+    `${process.env.URL_GOCARDLESS_TRANSACTIONS}/api/v1/transactions/transactionsDetails/:accountId`,
+    'GET',
+    {},
+    {}
+  );
+
+export default getTransactions;
